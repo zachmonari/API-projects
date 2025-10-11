e70c4d0e7e19fef5e153d180cebfa60a2dc355d9
zach <zachmonari002@gmail.com>
2025-10-11 10:10:58 +0300

    Added the POST feature

--- Fetch.py
+++ Fetch.py
@@ -13,6 +13,11 @@ def get_user(user_id):
         user_data["extra"] = extra
     return jsonify(user_data), 200
 
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
 
 
 if __name__ == '__main__':
