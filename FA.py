0a3ade13ef23b7cb8db955ed34de90696e657110
zach <zachmonari002@gmail.com>
2025-11-22 21:08:13 +0300

    Query

--- FA.py
+++ FA.py
@@ -6,6 +6,8 @@ app=FastAPI()
 def home():
     return {"message":"Hello FastAPI Learner!"}
 
+#GET Request with Path Parameter
 @app.get("/student/{student_id}")
 def get_student(student_id: int):
     return {"student_id": student_id}
+#GET with Query Parameter
