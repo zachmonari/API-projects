fb77413a646bac9deed8d23463f34c56316a1fe7
zach <zachmonari002@gmail.com>
2025-11-22 21:27:16 +0300

    Class

--- FA.py
+++ FA.py
@@ -17,3 +17,11 @@ get_student(1)
 def compute_energy(amplitude: float, mass: float):
     return {"energy": 0.5 * mass * amplitude**2}
 compute_energy(3.0, 3.0)
+
+class Book():
+    title: str
+    author: str
+    pages: int
+@app.post("/add-book")
+def add_book(book: Book):
+    return {"message": "Book added!", "data": book}
