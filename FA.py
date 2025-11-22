b950b5363cb24a122d517bc0e4a02e3282f5afa8
zach <zachmonari002@gmail.com>
2025-11-22 21:04:07 +0300

    student data

--- FA.py
+++ FA.py
@@ -5,3 +5,7 @@ app=FastAPI()
 @app.get("/")
 def home():
     return {"message":"Hello FastAPI Learner!"}
+
+@app.get("/student/{student_id}")
+def get_student(student_id: int):
+    return {"student_id": student_id}
