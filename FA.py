0a5b06f71edc8528651872d0891deefda4f614bb
zach <zachmonari002@gmail.com>
2025-11-23 15:52:53 +0300

    Read one

--- FA.py
+++ FA.py
@@ -61,3 +61,13 @@ def create_book(book: Book):
 @app.get("/books", response_model=List[Book])
 def get_all_books():
     return books_db
+
+# ------------------------
+# 3️⃣ READ ONE (GET by ID)
+# ------------------------
+@app.get("/books/{book_id}", response_model=Book)
+def get_book(book_id: int):
+    for book in books_db:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Book not found")
