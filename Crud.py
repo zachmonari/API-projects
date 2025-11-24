444b6f65a0a9d99487aaf6c0d1ba202aec108574
zach <zachmonari002@gmail.com>
2025-11-24 19:50:52 +0300

    Crud operations

---
+++ Crud.py
@@ -0,0 +1,43 @@
+from sqlalchemy.orm import Session
+import Models, Schemas
+
+def get_books(db: Session):
+    return db.query(Models.Book).all()
+
+def get_book(db: Session, book_id: int):
+    return db.query(Models.Book).filter(Models.Book.id == book_id).first()
+
+def create_book(db: Session, book: Schemas.BookCreate):
+    db_book = Models.Book(
+        title=book.title,
+        author=book.author,
+        pages=book.pages,
+        description=book.description
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+def update_book(db: Session, book_id: int, book: Schemas.BookCreate):
+    db_book = get_book(db, book_id)
+    if db_book is None:
+        return None
+
+    db_book.title = book.title
+    db_book.author = book.author
+    db_book.pages = book.pages
+    db_book.description = book.description
+
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+def delete_book(db: Session, book_id: int):
+    db_book = get_book(db, book_id)
+    if db_book is None:
+        return None
+
+    db.delete(db_book)
+    db.commit()
+    return db_book
