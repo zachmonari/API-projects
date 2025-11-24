1ea776fed5b51b2079e0107a00580cf2301a32f7
zach <zachmonari002@gmail.com>
2025-11-24 19:31:12 +0300

    Full example

--- FA.py
+++ FA.py
@@ -2,36 +2,11 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List
 
-app=FastAPI()
-
-@app.get("/")
-def home():
-    return {"message":"Hello FastAPI Learner!"}
-
-#GET Request with Path Parameter
-@app.get("/student/{student_id}")
-def get_student(student_id: int):
-    return {"student_id": student_id}
-
-
-#GET with Query Parameter
-@app.get("/vibration/energy")
-def compute_energy(amplitude: float, mass: float):
-    return {"energy": 0.5 * mass * amplitude**2}
-
-
-class Book(BaseModel):
-    title: str
-    author: str
-    pages: int
-@app.post("/add-book")
-def add_book(book: Book):
-    return {"message": "Book added!", "data": book}
-
 # Full example
 # ------------------------
 # DATA MODEL
 # ------------------------
+app=FastAPI()
 class Book(BaseModel):
     id: int
     title: str
