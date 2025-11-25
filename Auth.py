5d7f4ce978cdc7c1d3f3eb394111674c3f7c8080
zach <zachmonari002@gmail.com>
2025-11-25 20:58:57 +0300

    Load dotenv

--- Auth.py
+++ Auth.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -6,6 +8,7 @@ from passlib.context import CryptContext
 # Hashing settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
