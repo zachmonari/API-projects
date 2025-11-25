f6d66a1d71bd708d6b4c7e31e7f071aace32d20e
zach <zachmonari002@gmail.com>
2025-11-25 20:50:51 +0300

    Hashing settings

--- Auth.py
+++ Auth.py
@@ -1,3 +1,6 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+
+# Hashing settings
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
