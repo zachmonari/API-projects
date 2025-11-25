9946223babd46d5f6bd99b7c9af2a888028d2756
zach <zachmonari002@gmail.com>
2025-11-25 21:02:03 +0300

    Create access tokens

--- Auth.py
+++ Auth.py
@@ -19,3 +19,9 @@ def hash_password(password: str):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
