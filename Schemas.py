6c1a58dffc7f775443333694c0b649aacf1dc922
zach <zachmonari002@gmail.com>
2025-11-26 20:25:35 +0300

    model config

--- Schemas.py
+++ Schemas.py
@@ -5,3 +5,10 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
+class User(BaseModel):
+    id: int
+    username: str
+
+    model_config = {
+        "from_attributes": True
+    }
