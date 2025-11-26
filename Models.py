0ec6bc42dcd1f4d99fb320980f9f8084bd2128bf
zach <zachmonari002@gmail.com>
2025-11-26 20:23:03 +0300

    users

--- Models.py
+++ Models.py
@@ -1,11 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from Database import Base
 
-class Book(Base):
-    __tablename__ = "books"
+class User(Base):
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    author = Column(String)
-    pages = Column(Integer)
-    description = Column(String, nullable=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
