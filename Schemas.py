34de80caa6c420804fe5af50c103057895740285
zach <zachmonari002@gmail.com>
2025-11-26 20:24:32 +0300

    Basemodel

--- Schemas.py
+++ Schemas.py
@@ -1,19 +1,7 @@
 from pydantic import BaseModel
-from typing import Optional
 
-class BookBase(BaseModel):
-    title: str
-    author: str
-    pages: int
-    description: Optional[str] = None
 
-class BookCreate(BookBase):
-    pass
-
-class Book(BookBase):
-    id: int
-
-    model_config = {
-        "from_attributes": True
-    }
+class UserCreate(BaseModel):
+    username: str
+    password: str
 
