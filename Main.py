65f1cfd4f6c3aa0ad555c081a3797843790fd60b
zach <zachmonari002@gmail.com>
2025-11-26 20:31:49 +0300

    Auth integration

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from Auth import create_access_token, SECRET_KEY, ALGORITHM
+from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 import Models
 from Database import engine, get_db
@@ -10,36 +13,5 @@ Models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# CREATE
-@app.post("/books", response_model=Schemas.Book)
-def create_book(book: Schemas.BookCreate, db: Session = Depends(get_db)):
-    return Crud.create_book(db, book)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-# READ ALL
-@app.get("/books", response_model=list[Schemas.Book])
-def read_books(db: Session = Depends(get_db)):
-    return Crud.get_books(db)
-
-# READ ONE
-@app.get("/books/{book_id}", response_model=Schemas.Book)
-def read_book(book_id: int, db: Session = Depends(get_db)):
-    db_book = Crud.get_book(db, book_id)
-    if db_book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return db_book
-
-# UPDATE
-@app.put("/books/{book_id}", response_model=Schemas.Book)
-def update_book(book_id: int, book: Schemas.BookCreate, db: Session = Depends(get_db)):
-    updated = Crud.update_book(db, book_id, book)
-    if updated is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return updated
-
-# DELETE
-@app.delete("/books/{book_id}")
-def delete_book(book_id: int, db: Session = Depends(get_db)):
-    deleted = Crud.delete_book(db, book_id)
-    if deleted is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return {"message": "Book deleted successfully"}
