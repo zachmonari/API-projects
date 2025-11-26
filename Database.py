c417ea2e2b41fecdd1b905d8b889fc6ba373f370
zach <zachmonari002@gmail.com>
2025-11-26 20:20:45 +0300

    Sessions

--- Database.py
+++ Database.py
@@ -7,3 +7,6 @@ engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
