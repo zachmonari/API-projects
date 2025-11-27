d8d3d6ff4c1a1bcd9271230051b96dffa31bdd3f
zach <zachmonari002@gmail.com>
2025-11-27 11:05:10 +0300

    Book models

--- Models.py
+++ Models.py
@@ -1,9 +1,21 @@
-from sqlalchemy import Column, Integer, String
 from Database import Base
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+    pages = Column(Integer)
+    description = Column(String, nullable=True)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User")
