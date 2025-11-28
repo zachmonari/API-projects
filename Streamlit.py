ca51c1c3acdbfdb5204e4a438d73edc565980dc6
zach <zachmonari002@gmail.com>
2025-11-28 20:49:22 +0300

    auth functions

--- Streamlit.py
+++ Streamlit.py
@@ -7,3 +7,16 @@ API_URL = "http://localhost:8000"  # FastAPI URL
 if "token" not in st.session_state:
     st.session_state["token"] = None
 
+# ---- AUTH FUNCTIONS ----
+def register_user(username, password):
+    payload = {"username": username, "password": password}
+    response = requests.post(f"{API_URL}/auth/register", json=payload)
+    return response
+
+
+def login_user(username, password):
+    payload = {"username": username, "password": password}
+    response = requests.post(f"{API_URL}/auth/login", data=payload)
+    if response.status_code == 200:
+        st.session_state["token"] = response.json()["access_token"]
+    return response
