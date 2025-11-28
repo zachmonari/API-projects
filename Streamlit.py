9d2dd270d11e3ccd9ad62abe329fb617c7cc456c
zach <zachmonari002@gmail.com>
2025-11-28 20:50:36 +0300

    Book functions

--- Streamlit.py
+++ Streamlit.py
@@ -20,3 +20,23 @@ def login_user(username, password):
     if response.status_code == 200:
         st.session_state["token"] = response.json()["access_token"]
     return response
+
+# ---- BOOK FUNCTIONS ----
+def get_books():
+    headers = {"Authorization": f"Bearer {st.session_state['token']}"}
+    return requests.get(f"{API_URL}/books/", headers=headers)
+
+
+def create_book(data):
+    headers = {"Authorization": f"Bearer {st.session_state['token']}"}
+    return requests.post(f"{API_URL}/books/", json=data, headers=headers)
+
+
+def update_book(book_id, data):
+    headers = {"Authorization": f"Bearer {st.session_state['token']}"}
+    return requests.put(f"{API_URL}/books/{book_id}", json=data, headers=headers)
+
+
+def delete_book(book_id):
+    headers = {"Authorization": f"Bearer {st.session_state['token']}"}
+    return requests.delete(f"{API_URL}/books/{book_id}", headers=headers)
