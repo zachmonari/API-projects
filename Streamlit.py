b88187c24eb68c66d09e34f7d52a301feaebc0fd
zach <zachmonari002@gmail.com>
2025-11-28 20:25:19 +0300

    Session state

--- Streamlit.py
+++ Streamlit.py
@@ -1,2 +1,9 @@
 import requests
-import streamlit
+import streamlit as st
+
+API_URL = "http://localhost:8000"  # FastAPI URL
+
+# ---- SESSION STATE ----
+if "token" not in st.session_state:
+    st.session_state["token"] = None
+
